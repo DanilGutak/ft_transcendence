a5fbd118975f10a0f027ebaa272f8b0121b995bb
Danil GUTAK <danil.gutak@rbinternational.com>
2024-06-13 21:27:06 +0200

    added postgres for db

--- login/login/settings.py
+++ login/login/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'login.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'login', 
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
