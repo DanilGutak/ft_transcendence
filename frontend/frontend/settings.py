9e7f14ee50f7ea53c7a8e3dc9ab27ab73c157e1b
Ruslan Basyrov <rusruo@gmail.com>
2024-07-24 14:43:04 +0200

    style elements as separate css files and static volume is now shared between host and the container

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -77,12 +77,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Additional locations of static files
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Ensure you have the following security settings when DEBUG is False
-DEBUG = False
+DEBUG = True
 
 WSGI_APPLICATION = 'frontend.wsgi.application'
 
@@ -137,3 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import mimetypes
+mimetypes.add_type("frontend_app/templates/css", ".css", True)
